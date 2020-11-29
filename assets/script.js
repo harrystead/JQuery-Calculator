6c39b908b1d2155817641ea5061ae8c20fafc363
Harry Stead <harrystead9@gmail.com>
2020-11-29 11:17:53 +0000

    first num and second num

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,17 @@ $(document).ready(function() {
     var operator = "";
     var operatorChosen = false;
     var resultEl = $("#result");
+
+    $(".number").on("click", function (){
+        var number = $(this).val();
+    
+        if(operatorChosen === false) {
+          firstNumString += number;
+          $("#first-number").text(firstNumString);
+    
+        }
+        else if(operatorChosen === true) {
+          secondNumString += number;
+          $("#second-number").text(secondNumString);
+        }
+      })
